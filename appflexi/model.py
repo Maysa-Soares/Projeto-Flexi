99d919bd9aea4d86334a75ea23f8fa799d351c92
maysa soares <maysasoares092004@gmail.com>
2025-11-24 13:52:28 -0300

    chore: database configuration and modeling.

--- appflexi/model.py
+++ appflexi/model.py
@@ -1,7 +1,19 @@
+from email.policy import default
+from enum import unique
+
 from appflexi import database
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 class User(database.Model):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(20),unique=True ,nullable=False)
+    email = database.Column(database.String(100),unique=True ,nullable=False)
+    password = database.Column(database.String(60),nullable=False)
+    photos = database.relationship('Photo', backref='user', lazy=True)
 
-class photo(database.Model):
-    pass
+class Photo(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    file_name =database.Column(database.String(255),default="default.png")
+    upload_date = database.Column(database.DateTime,default=lambda: datetime.now(ZoneInfo('America/Sao Paulo')),nullable=False)
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
--- create_database.py
+++ create_database.py
@@ -1,4 +1,5 @@
 from appflexi import database, app
+from appflexi.model import Photo, User
 
 with app.app_context():
     database.create_all()
